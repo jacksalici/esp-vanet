45515b1d62f7bbc34af69597c6693a7dd7edb755
Giacomo Salici <giambo2000@gmail.com>
2024-03-06 01:02:41 +0100

    update

--- main/main.ino
+++ main/main.ino
@@ -4,10 +4,9 @@
 
 BluetoothSerial SerialBT;
 
-
 #define DEBUG_PORT Serial
-#define ELM_PORT   SerialBT
-uint8_t address[6] = { 0x00, 0x10, 0xCC, 0x4F, 0x36, 0x03 };
+#define ELM_PORT SerialBT
+uint8_t address[6] = {0x00, 0x10, 0xCC, 0x4F, 0x36, 0x03};
 float rpm = 0;
 
 ELM327 myELM327;
@@ -24,27 +23,24 @@ void setup()
   if (!ELM_PORT.connect(address))
   {
     DEBUG_PORT.println("Couldn't connect to OBD scanner");
-    while(1);
+    while (1)
+      ;
   }
 
   DEBUG_PORT.println("Connected to ELM327");
   myELM327.begin(ELM_PORT, true, 2000);
 }
 
-
 void loop()
 {
-  rpm = myELM327.rpm();
-      
-    if (myELM327.nb_rx_state == ELM_SUCCESS)
-    {
-      DEBUG_PORT.print("rpm: ");
-      DEBUG_PORT.println(rpm);
-    }
-    else
-    {
-      DEBUG_PORT.println("ERROR");
-      myELM327.printError();
-      
-    }
+  float tempRPM = myELM327.rpm();
+
+  if (myELM327.nb_rx_state == ELM_SUCCESS)
+  {
+    rpm = (uint32_t)tempRPM;
+    DEBUG_PORT.print("RPM: ");
+    DEBUG_PORT.println(rpm);
+  }
+  else if (myELM327.nb_rx_state != ELM_GETTING_MSG)
+    myELM327.printError();
 }
